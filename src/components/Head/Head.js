440012c8ccff44281965c2d91769fcfda0257c2c
Anthony Caron <antcar1992@gmail.com>
2019-12-20 22:13:00 -0800

    theme-color added

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 const Head = ({ pageName }) => (
 	<Helmet>
 		<title>Cody Cameron - {pageName}</title>
+		<meta name="theme-color" content="#000000" />
 	</Helmet>
 );
 
